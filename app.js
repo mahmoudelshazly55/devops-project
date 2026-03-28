5a5829e224bd7aaa63a3441762c7fbf1cd65f546
Mahmoud Elshazly <mahmoudelshazly55@gmail.com>
2026-03-28 21:06:08 +0200

    connect frontend

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello DevOps 🚀");
-});
-
 const PORT = process.env.PORT || 3000;
+
+// مهم: يخلي السيرفر يقرأ الملفات من public
+app.use(express.static("public"));
+
+// الصفحة الرئيسية
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
